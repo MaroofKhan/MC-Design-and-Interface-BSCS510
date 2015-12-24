a0fea1fffc73bba9a906b2c2f6712ffb4f461f07
Maroof Khan <maroof-khan@live.com>
2015-12-24 11:44:41 +0500

    project basics

---
+++ Arduino - Project Basics/Controlling Servo/Controlling Servo.ino
@@ -0,0 +1,28 @@
+/*
+Instructions:
+* Google 'Servo Motor'
+*/
+
+
+#include <Servo.h>
+
+Servo testServo;
+int testPin = 9;
+
+void setup() {
+  testServo.attach(testPin);
+}
+
+void loop() {
+  
+  for (int pos = 0; pos <= 180; pos += 1) {
+    testServo.write(pos);
+    delay(15);
+  }
+  
+  for (int pos = 180; pos >= 0; pos -= 1) {
+    testServo.write(pos);
+    delay(15);
+  }
+
+}
